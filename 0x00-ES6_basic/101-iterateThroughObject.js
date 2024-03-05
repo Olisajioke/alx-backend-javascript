170ab716e7081003687a94bcf58342edc7fef083
Olisajioke <chijioke914@gmail.com>
2024-03-05 12:16:57 +0100

    task 14

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,14 @@
-export default function iterateThroughObject(reportWithIterator) {
-    let result = '';
-    for (const item of reportWithIterator) {
-      result += item + ' | ';
+export default function createReportObject(employeesList) {
+    const allEmployees = {};
+  
+    for (const [departmentName, employees] of Object.entries(employeesList)) {
+      allEmployees[departmentName] = [...employees];
     }
-
-    if (result.length > 0) {
-      result = result.slice(0, -3);
-    }
-    return result;
+  
+    return {
+      allEmployees,
+      getNumberOfDepartments() {
+        return Object.keys(employeesList).length;
+      },
+    };
   }
