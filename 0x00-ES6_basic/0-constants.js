9db4dcf45d98a8b6ca24247908e97b77bcb15c88
Olisajioke <chijioke914@gmail.com>
2024-03-05 11:19:59 +0100

    task 0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,5 @@
-// Use `export` before the function declaration to export them
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Changed `var` to `const`
+  const task = 'I prefer const when I can.';
   return task;
 }
 
@@ -9,7 +8,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let'; // Changed `var` to `let`
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
