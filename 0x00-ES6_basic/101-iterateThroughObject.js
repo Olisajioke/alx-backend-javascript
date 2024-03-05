2e14be9097815e3c56dbd85d1a84e9879d906ca3
Olisajioke <chijioke914@gmail.com>
2024-03-05 12:08:03 +0100

    task 13 and 14

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,11 @@
+export default function iterateThroughObject(reportWithIterator) {
+    let result = '';
+    for (const item of reportWithIterator) {
+      result += item + ' | ';
+    }
+
+    if (result.length > 0) {
+      result = result.slice(0, -3);
+    }
+    return result;
+  }
