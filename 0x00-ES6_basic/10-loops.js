b3a168c24822166e06f78372c4402fd249324d0f
Olisajioke <chijioke914@gmail.com>
2024-03-05 12:11:33 +0100

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
-export default function appendToEachArrayValue(array, appendString) {
-    for (let value of array) {
-      value = appendString + value;
+  export default function appendToEachArrayValue(array, appendString) {
+    const arra = [];
+    for (const val of array) {
+      arra.push(appendString + val);
     }
   
-    return array;
+    return arra;
   }
-  
