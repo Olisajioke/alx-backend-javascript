5503748a0029884b766ee985f8704dd34cc04eef
Olisajioke <chijioke914@gmail.com>
2024-03-05 12:14:01 +0100

    task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,26 +1,8 @@
 export default function createIteratorObject(report) {
-    const departments = Object.values(report.allEmployees);
-    let currentDepartmentIndex = 0;
-    let currentEmployeeIndex = 0;
+    const arr = [];
+    for (const item of Object.values(report.allEmployees)) {
+      arr.push(...item);
+    }
   
-    return {
-      next() {
-        if (currentDepartmentIndex >= departments.length) {
-          return { done: true };
-        }
-  
-        const currentDepartment = departments[currentDepartmentIndex];
-        const currentEmployee = currentDepartment[currentEmployeeIndex];
-  
-        if (currentEmployeeIndex >= currentDepartment.length - 1) {
-          currentDepartmentIndex++;
-          currentEmployeeIndex = 0;
-        } else {
-          currentEmployeeIndex++;
-        }
-  
-        return { value: currentEmployee, done: false };
-      }
-    };
+    return arr[Symbol.iterator]();
   }
-  
