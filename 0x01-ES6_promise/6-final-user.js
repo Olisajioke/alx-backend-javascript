f947af93f6cc3d4be56ef409ba9cf0f1e2e08814
Olisajioke <chijioke914@gmail.com>
2024-03-07 07:38:34 +0000

    task 10

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,9 +3,8 @@ import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(results => results.map(({ status, value, reason }) => ({
+    .then((results) => results.map(({ status, value, reason }) => ({
       status: status === 'fulfilled' ? 'success' : 'failure',
-      value: status === 'fulfilled' ? value : reason
+      value: status === 'fulfilled' ? value : reason,
     })));
 }
-
