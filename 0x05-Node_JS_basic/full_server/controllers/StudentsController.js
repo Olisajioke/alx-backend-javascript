d607d06ee545ccc2842b083f82a9be7854cb4d96
Olisajioke <chijioke914@gmail.com>
2024-04-29 09:38:14 +0100

    task 8

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,39 @@
+import { readDatabase } from '../utils.js';
+
+class StudentsController {
+  static async getAllStudents(req, res) {
+    try {
+      const students = await readDatabase('./database.csv');
+      let response = 'This is the list of our students\n';
+
+      Object.keys(students).sort((a, b) => a.localeCompare(b)).forEach(field => {
+        const count = students[field].length;
+        const list = students[field].join(', ');
+        response += `Number of students in ${field}: ${count}. List: ${list}\n`;
+      });
+
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    const { major } = req.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    try {
+      const students = await readDatabase('./database.csv');
+      const list = students[major] ? students[major].join(', ') : '';
+      res.status(200).send(`List: ${list}`);
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+}
+
+export default StudentsController;
